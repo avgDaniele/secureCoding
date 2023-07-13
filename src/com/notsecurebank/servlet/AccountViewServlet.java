ea3b8a09acfed1ceded460e9be856e510d0b6a73
Daniele Raveglia <d.raveglia@reply.it>
2023-07-13 15:24:20 +0200

    Fix v2_LOG_INJECTION

--- src/com/notsecurebank/servlet/AccountViewServlet.java
+++ src/com/notsecurebank/servlet/AccountViewServlet.java
@@ -50,6 +50,9 @@ public class AccountViewServlet extends HttpServlet {
             String startTime = request.getParameter("startDate");
             String endTime = request.getParameter("endDate");
 
+            startTime = StringEscapeUtils.escapeSql(startTime);
+            endTime = StringEscapeUtils.escapeSql(endTime);
+
             LOG.info("Transactions within '" + startTime + "' and '" + endTime + "'.");
             RequestDispatcher dispatcher = request.getRequestDispatcher("/bank/transaction.jsp?" + ((startTime != null) ? "&startTime=" + startTime : "") + ((endTime != null) ? "&endTime=" + endTime : ""));
             dispatcher.forward(request, response);
